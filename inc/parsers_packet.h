3219b8b46069305b50f0316624e1acae1d7783b9
Polina Kurkina <polinadk9@gmail.com>
2025-08-13 12:05:29 +0300

    parsers of packet data moved to src/

---
+++ inc/parsers_packet.h
@@ -0,0 +1,16 @@
+#ifndef PARSERS_PACKET_H
+#define PARSERS_PACKET_H
+#include "filter.h"
+#include "definitions.h"
+void
+parse_packet_ipv4(char const *buffer, size_t bufflen, struct filter *packet_data, size_t const eth_hdr_len);
+
+void
+parse_packet_ipv6(char const *buffer, size_t bufflen, struct filter *packet_data);
+
+void
+parse_packet_vlan(char const *buffer, size_t bufflen, struct filter *packet_data);
+
+void
+parse_packet_ether(char const *buffer, size_t bufflen, struct filter *packet_data);
+#endif
