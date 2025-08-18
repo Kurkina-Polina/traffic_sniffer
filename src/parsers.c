e330c2422c04522e75619d40fcf772fa6cfd18fd
Polina Kurkina <polinadk9@gmail.com>
2025-08-18 17:18:17 +0300

    change parsers

--- src/parsers.c
+++ src/parsers.c
@@ -20,21 +20,19 @@ parse_mac(const char *str, struct ether_addr *mac)
 bool
 parse_dst_mac(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "dst_mac") == 0) && (new_filter->flags.dst_mac_flag != 0))
+    if (strcmp(name_key, "dst_mac") != 0)
+        return true;
+    if (new_filter->flags.dst_mac_flag)
     {
         strncpy(message, "Error: dst mac is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "dst_mac") == 0)
-        {
-            struct ether_addr *mac = &(new_filter->dst_mac);
-            if (!parse_mac(val_key, mac))
-            {
-                strncpy(message, "Error: filter dst_mac \n", sizeof(message));
-                return false;
-            }
-            new_filter->flags.dst_mac_flag = 1;
-        }
+    if (!parse_mac(val_key, &new_filter->dst_mac))
+    {
+        strncpy(message, "Error: filter dst_mac \n", sizeof(message));
+        return false;
+    }
+    new_filter->flags.dst_mac_flag = true;
     return true;
 }
 
@@ -43,47 +41,42 @@ parse_dst_mac(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_src_mac(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    //FIXME: don't use copy paste strings twice
-    if ((strcmp(name_key, "src_mac") == 0) && (new_filter->flags.src_mac_flag != 0))
-    {
+    if (strcmp(name_key, "src_mac") != 0)
+        return true;
+    if (new_filter->flags.src_mac_flag) {
         strncpy(message, "Error: src mac is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "src_mac") == 0)
+    if (!parse_mac(val_key, &new_filter->src_mac))
     {
-        struct ether_addr *mac = &(new_filter->src_mac);
-        if (!parse_mac(val_key, mac))
-        {
-            strncpy(message, "Error: filter src_mac \n", sizeof(message));
-            return false;
-        }
-        new_filter->flags.src_mac_flag = true;
+        strncpy(message, "Error: filter src_mac \n", sizeof(message));
+        return false;
     }
+    new_filter->flags.src_mac_flag = true;
     return true;
 }
 
 
-/* Parsesrc ipv4 from string val_key to field of struct new_filter. */
+/* Parse src ipv4 from string val_key to field of struct new_filter. */
 bool
 parse_src_ipv4(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "src_ipv4") == 0) && (new_filter->flags.src_ipv4_flag != 0))
+    if (strcmp(name_key, "src_ipv4") != 0)
+        return true;
+    if (new_filter->flags.src_ipv4_flag)
     {
         strncpy(message, "Error: src ipv4 is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "src_ipv4") == 0)
-        {
-            int result = inet_pton(AF_INET, val_key, &(new_filter->src_ipv4));
-            if (result<=0) {
-                printf("error: Not in presentation format");
-                printf("%s|%s\n",
-                    val_key, inet_ntoa(new_filter->src_ipv4));
-                strncpy(message, "Error: filter src_ipv4: not in presentation format\n", sizeof(message));
-                return false;
-            }
-            new_filter->flags.src_ipv4_flag = 1;
-        }
+    int result = inet_pton(AF_INET, val_key, &(new_filter->src_ipv4));
+    if (result<=0) {
+        printf("error: Not in presentation format");
+        printf("%s|%s\n",
+            val_key, inet_ntoa(new_filter->src_ipv4));
+        strncpy(message, "Error: filter src_ipv4: not in presentation format\n", sizeof(message));
+        return false;
+    }
+    new_filter->flags.src_ipv4_flag = true;
     return true;
 }
 
@@ -92,23 +85,22 @@ parse_src_ipv4(const char *name_key, const char *val_key, struct filter *new_fil
 bool
 parse_dst_ipv4(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "dst_ipv4") == 0) && (new_filter->flags.dst_ipv4_flag != 0))
+    if (strcmp(name_key, "dst_ipv4") != 0)
+        return true;
+    if (new_filter->flags.dst_ipv4_flag)
     {
         strncpy(message, "Error: dst ipv4 is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "dst_ipv4") == 0)
-        {
-            int result = inet_pton(AF_INET, val_key, &(new_filter->dst_ipv4));
-            if (result <= 0) {
-                printf("error: Not in presentation format");
-                printf("%s|%s\n",
-                    val_key, inet_ntoa(new_filter->dst_ipv4));
-                strncpy(message, "Error: filter dst_ipv4: not in presentation format\n", sizeof(message));
-                return false;
-            }
-            new_filter->flags.dst_ipv4_flag = true;
-        }
+    int result = inet_pton(AF_INET, val_key, &(new_filter->dst_ipv4));
+    if (result <= 0) {
+        printf("error: Not in presentation format");
+        printf("%s|%s\n",
+            val_key, inet_ntoa(new_filter->dst_ipv4));
+        strncpy(message, "Error: filter dst_ipv4: not in presentation format\n", sizeof(message));
+        return false;
+    }
+    new_filter->flags.dst_ipv4_flag = true;
     return true;
 }
 
@@ -117,22 +109,21 @@ parse_dst_ipv4(const char *name_key, const char *val_key, struct filter *new_fil
 bool
 parse_src_ipv6(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "src_ipv6") == 0) && (new_filter->flags.src_ipv6_flag != 0))
+    if (strcmp(name_key, "src_ipv6") != 0)
+        return true;
+    if (new_filter->flags.src_ipv6_flag)
     {
         strncpy(message, "Error: src ipv6 is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "src_ipv6") == 0)
-    {
-        int result = inet_pton(AF_INET6, val_key, &(new_filter->src_ipv6));
-        if (result<=0) {
-            printf("error: Not in presentation format");
-            printf("|%s|\n",val_key);
-            strncpy(message, "Error: filter src_ipv6: not in presentation format\n", sizeof(message));
-            return false;
-        }
-        new_filter->flags.src_ipv6_flag = 1;
+    int result = inet_pton(AF_INET6, val_key, &(new_filter->src_ipv6));
+    if (result<=0) {
+        printf("error: Not in presentation format");
+        printf("|%s|\n",val_key);
+        strncpy(message, "Error: filter src_ipv6: not in presentation format\n", sizeof(message));
+        return false;
     }
+    new_filter->flags.src_ipv6_flag = 1;
     return true;
 }
 
@@ -141,22 +132,21 @@ parse_src_ipv6(const char *name_key, const char *val_key, struct filter *new_fil
 bool
 parse_dst_ipv6(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "dst_ipv6") == 0) && (new_filter->flags.dst_ipv6_flag != 0))
+    if (strcmp(name_key, "dst_ipv6") != 0)
+        return true;
+    if (new_filter->flags.dst_ipv6_flag)
     {
         strncpy(message, "Error: dst ipv6 is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "dst_ipv6") == 0)
-    {
-        int result = inet_pton(AF_INET6, val_key, &(new_filter->dst_ipv6));
-        if (result<=0) {
-            printf("error: Not in presentation format");
-            printf("|%s|\n",val_key);
-            strncpy(message, "Error: filter dst_ipv6: not in presentation format\n", sizeof(message));
-            return false;
-        }
-        new_filter->flags.dst_ipv6_flag = 1;
+    int result = inet_pton(AF_INET6, val_key, &(new_filter->dst_ipv6));
+    if (result<=0) {
+        printf("error: Not in presentation format");
+        printf("|%s|\n",val_key);
+        strncpy(message, "Error: filter dst_ipv6: not in presentation format\n", sizeof(message));
+        return false;
     }
+    new_filter->flags.dst_ipv6_flag = 1;
     return true;
 }
 
@@ -165,23 +155,22 @@ parse_dst_ipv6(const char *name_key, const char *val_key, struct filter *new_fil
 bool
 parse_ip_protocol(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "ip_protocol") == 0) && (new_filter->flags.ip_protocol_flag != 0))
+    if (strcmp(name_key, "ip_protocol") != 0)
+        return true;
+    if (new_filter->flags.ip_protocol_flag)
     {
         strncpy(message, "Error: ip protocol is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "ip_protocol") == 0)
-        {
-            errno = 0;
-            uint8_t ip_protocol = (uint8_t)strtoul(val_key, NULL, 0);
-            if (errno != 0)
-            {
-                strncpy(message, "Error: ip protocol not in presenttion format\n", sizeof(message));
-                return false;
-            }
-            new_filter->ip_protocol = ip_protocol;
-            new_filter->flags.ip_protocol_flag = 1;
-        }
+    errno = 0;
+    uint8_t ip_protocol = (uint8_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
+    {
+        strncpy(message, "Error: ip protocol not in presenttion format\n", sizeof(message));
+        return false;
+    }
+    new_filter->ip_protocol = ip_protocol;
+    new_filter->flags.ip_protocol_flag = 1;
     return true;
 }
 
@@ -190,23 +179,23 @@ parse_ip_protocol(const char *name_key, const char *val_key, struct filter *new_
 bool
 parse_ether_type(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "ether_type") == 0) && (new_filter->flags.ether_type_flag != 0))
+    if (strcmp(name_key, "ether_type") != 0)
+        return true;
+    if (new_filter->flags.ether_type_flag)
     {
         strncpy(message, "Error: ether type is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "ether_type") == 0)
-        {
-            errno = 0;
-            uint16_t ether_type = (uint16_t)strtoul(val_key, NULL, 0);
-            if (errno != 0)
-            {
-                strncpy(message, "Error: ether type not in presenttion format\n", sizeof(message));
-                return false;
-            }
-            new_filter->ether_type = htons(ether_type);
-            new_filter->flags.ether_type_flag = 1;
-        }
+    errno = 0;
+    uint16_t ether_type = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
+    {
+        strncpy(message, "Error: ether type not in presenttion format\n", sizeof(message));
+        return false;
+    }
+    new_filter->ether_type = htons(ether_type);
+    new_filter->flags.ether_type_flag = 1;
+
     return true;
 }
 
@@ -215,22 +204,21 @@ parse_ether_type(const char *name_key, const char *val_key, struct filter *new_f
 bool
 parse_src_tcp(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "src_tcp") == 0) && (new_filter->flags.src_tcp_flag != 0)){
+    if (strcmp(name_key, "src_tcp") != 0)
+        return true;
+    if (new_filter->flags.src_tcp_flag){
         strncpy(message, "Error: src tcp is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "src_tcp") == 0)
+    errno = 0;
+    uint16_t src_tcp = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
     {
-        errno = 0;
-        uint16_t src_tcp = (uint16_t)strtoul(val_key, NULL, 0);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: src tcp not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->src_tcp = htons(src_tcp);
-        new_filter->flags.src_tcp_flag = 1;
+        strncpy(message, "Error: src tcp not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->src_tcp = htons(src_tcp);
+    new_filter->flags.src_tcp_flag = 1;
     return true;
 }
 
@@ -239,22 +227,21 @@ parse_src_tcp(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_dst_tcp(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "dst_tcp") == 0) && (new_filter->flags.dst_tcp_flag != 0)){
+    if (strcmp(name_key, "dst_tcp") != 0)
+        return true;
+    if (new_filter->flags.dst_tcp_flag){
         strncpy(message, "Error: dst tcp is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "dst_tcp") == 0)
+    errno = 0;
+    uint16_t dst_tcp = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
     {
-        errno = 0;
-        uint16_t dst_tcp = (uint16_t)strtoul(val_key, NULL, 0);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: dst tcp not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->dst_tcp = htons(dst_tcp);
-        new_filter->flags.dst_tcp_flag = 1;
+        strncpy(message, "Error: dst tcp not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->dst_tcp = htons(dst_tcp);
+    new_filter->flags.dst_tcp_flag = 1;
     return true;
 }
 
@@ -263,22 +250,21 @@ parse_dst_tcp(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_src_udp(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "src_udp") == 0) && (new_filter->flags.src_udp_flag != 0)){
+    if (strcmp(name_key, "src_udp") != 0)
+        return true;
+    if (new_filter->flags.src_udp_flag){
         strncpy(message, "Error: src udp is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "src_udp") == 0)
+    errno = 0;
+    uint16_t src_udp = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
     {
-        errno = 0;
-        uint16_t src_udp = (uint16_t)strtoul(val_key, NULL, 0);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: src udp not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->src_udp = htons(src_udp);
-        new_filter->flags.src_udp_flag = 1;
+        strncpy(message, "Error: src udp not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->src_udp = htons(src_udp);
+    new_filter->flags.src_udp_flag = 1;
     return true;
 }
 
@@ -286,22 +272,21 @@ parse_src_udp(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_dst_udp(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "dst_udp") == 0) && (new_filter->flags.dst_udp_flag != 0)){
+    if (strcmp(name_key, "dst_udp") != 0)
+        return true;
+    if (new_filter->flags.dst_udp_flag){
         strncpy(message, "Error: dst udp is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "dst_udp") == 0)
+    errno = 0;
+    uint16_t dst_udp = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
     {
-        errno = 0;
-        uint16_t dst_udp = (uint16_t)strtoul(val_key, NULL, 0);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: dst udp not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->dst_udp = htons(dst_udp);
-        new_filter->flags.dst_udp_flag = 1;
+        strncpy(message, "Error: dst udp not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->dst_udp = htons(dst_udp);
+    new_filter->flags.dst_udp_flag = 1;
     return true;
 }
 
@@ -310,22 +295,21 @@ parse_dst_udp(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_vlan_id(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "vlan_id") == 0) && (new_filter->flags.vlan_id_flag != 0)){
+    if (strcmp(name_key, "vlan_id") != 0)
+        return true;
+    if (new_filter->flags.vlan_id_flag){
         strncpy(message, "Error: vlan flag is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "vlan_id") == 0)
+    errno = 0;
+    uint16_t vlan_id = (uint16_t)strtoul(val_key, NULL, 0);
+    if (errno != 0)
     {
-        errno = 0;
-        uint16_t vlan_id = (uint16_t)strtoul(val_key, NULL, 0);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: vlan id not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->vlan_id = vlan_id;
-        new_filter->flags.vlan_id_flag = 1;
+        strncpy(message, "Error: vlan id not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->vlan_id = vlan_id;
+    new_filter->flags.vlan_id_flag = 1;
     return true;
 }
 
@@ -334,21 +318,20 @@ parse_vlan_id(const char *name_key, const char *val_key, struct filter *new_filt
 bool
 parse_interface(const char *name_key, const char *val_key, struct filter *new_filter, char *message)
 {
-    if ((strcmp(name_key, "interface") == 0) && (new_filter->flags.interface_flag != 0)){
+    if (strcmp(name_key, "interface") != 0)
+        return true;
+    if (new_filter->flags.interface_flag){
         strncpy(message, "Error: interface is set already \n", sizeof(message));
         return false;
     }
-    else if (strcmp(name_key, "interface") == 0)
+    errno = 0;
+    int interface = if_nametoindex(val_key);
+    if (errno != 0)
     {
-        errno = 0;
-        int interface = if_nametoindex(val_key);
-        if (errno != 0)
-        {
-            strncpy(message, "Error: interface not in presenttion format\n", sizeof(message));
-            return false;
-        }
-        new_filter->interface = interface;
-        new_filter->flags.interface_flag = 1;
+        strncpy(message, "Error: interface not in presenttion format\n", sizeof(message));
+        return false;
     }
+    new_filter->interface = interface;
+    new_filter->flags.interface_flag = 1;
     return true;
 }
